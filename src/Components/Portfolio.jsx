98fec97ab8942126386e866890df07933a9140f7
Himanshu <himanshupandey2016@gmail.com>
2024-07-24 00:21:01 +0530

    changes protfolio

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -4,15 +4,43 @@ import "./Portfolio.css";
 
 const portfolio = () => {
   const soloProjects = [
+    // {
+    //   id: 1,
+    //   title: "E-Commerce Backend Project with Node.js",
+    //   img: IMG1,
+    //   description: "DashBoard For Managing Product",
+    //   technologies: "Javascript | Nodejs | Express js | Mongodb | React Js",
+    //   link: "https://github.com/himpan1998/Ecommerce_Dashboard",
+    //   github: "https://github.com/himpan1998/Ecommerce_Dashboard",
+    // },
     {
-      id: 1,
-      title: "E-Commerce Backend Project with Node.js",
+      id: 2,
+      title: "BMI Calculator with React.js",
       img: IMG1,
-      description: "DashBoard For Managing Product",
-      technologies: "Javascript |Nodejs| Express js |Mongodb|Html|CSS| React Js",
-      link: "https://github.com/himpan1998/Ecommerce_Dashboard",
-      github: "https://github.com/himpan1998/Ecommerce_Dashboard",
+      description: "BMI Calculator",
+      technologies: "Javascript |  React Js | Hooks:useState | UseMemo",
+      link: "https://bmicalculatorhimanshu.netlify.app/",
+      github: "https://github.com/himpan1998/BMI-Calculator",
     },
+    {
+      id: 3,
+      title: "Calculator with React.js",
+      img: IMG1,
+      description: "Calculator",
+      technologies: "Javascript |  React Js | Hooks:useState",
+      link: "https://calculatorhimanshu.netlify.app/",
+      github: "https://github.com/himpan1998/Calculator",
+    },
+    {
+      id: 4,
+      title: "Expense-Tracker with React.js",
+      img: IMG1,
+      description: "Expense-Tracker",
+      technologies: "Javascript |  React Js | Hooks:useState",
+      link: "https://expensetracerhimanshu.netlify.app/",
+      github: "https://github.com/himpan1998/Expense-Tracker.git",
+    },
+    
   ];
 
   return (
@@ -52,7 +80,10 @@ const portfolio = () => {
           </article>
         ))}
       </div>
+      
     </section>
+    
+    
   );
 };
 
